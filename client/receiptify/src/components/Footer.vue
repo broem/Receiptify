5559b43ca26257376cbde66d01ce4229ad5721d6
Bleach <wacbleach@gmail.com>
2020-09-20 01:06:32 -0400

    image previews, remove some stuff

--- client/receiptify/src/components/Footer.vue
+++ client/receiptify/src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
  <v-footer class="pa-3 white--text" color="grey" absolute>
    <div>
-     Developed with ❤️  by Broem &copy; {{ new Date().getFullYear() }}
+     Developed with ❤️  by 
+     <a href="https://www.github.com/broem" target="_blank">Broem</a> 
+     &copy; {{ new Date().getFullYear() }}
    </div>
  </v-footer>
 </template>
--- client/receiptify/src/components/Home.vue
+++ client/receiptify/src/components/Home.vue
@@ -1,13 +1,33 @@
 <template>
-    <h1>Receiptify</h1>
+  <v-container>
+    <div class="file-upload-form">
+      Upload an image:
+      <input type="file" @change="previewImage" accept="image/*">
+    </div>
+    <div class="image-preview" v-if="imageData.length > 0">
+      <img class="preview" :src="imageData">
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'Home'
+  name: 'Home',
+  data: () => ({
+    imageData: ''
+  }),
+  methods: {
+  previewImage: function(event) {
+    var input = event.target;
+    if (input.files && input.files[0]) {
+
+      var reader = new FileReader();
+      reader.onload = (e) => {
+        this.imageData = e.target.result;
+    }
+    reader.readAsDataURL(input.files[0]);
+    }
+  }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
