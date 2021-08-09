8790dab129605f43f1fdd506f34f17cdee29205b
dlm1337 <61361992+dlm1337@users.noreply.github.com>
2021-08-09 17:16:47 -0400

    Update Landing.vue

--- client/receiptify/src/components/Landing.vue
+++ client/receiptify/src/components/Landing.vue
@@ -3,9 +3,9 @@
     <div class="logo"><img src="@/assets/RLogo.png" /></div>
     <div class="nav">
       <v-row no-gutters>
-      <v-col style="width:100%" v-for="(k,i) in navBar.title" :key="i" cols="12" sm="2">
-        <v-card class="pa-2" @click="pushPath(i)" outlined tile>
-          {{k}} 
+      <v-col v-for="(k,i) in navBar.title" :key="i" cols="12" sm="2">
+        <v-card class="pa-2 nav_card" @click="pushPath(i)">
+            <div class="nav_card_text">{{k}}</div> 
         </v-card>
       </v-col>
       <v-row>
@@ -53,20 +53,33 @@ export default {
 
 <style>
 .landing {
-  position: relative;
-  margin: 40px;
-  padding: 20px;
-  width: 100%;
-  height: 100%;
+  position: relative; 
+  width: 100vh;
+  height: 100vh;
 }
 
-.nav {
-  border: 2px solid red;
-  position: absolute;
+.nav { 
+  position: absolute; 
   width: 100vh;
-  height: 100px;
-  left: 50%;
-  top: 0%;
+  height: 100px; 
+  left: 57.5%;
+}
+
+.nav_card {
+  background-image: linear-gradient(
+    to right bottom,
+    rgba(10, 15, 15, 0.774),
+    rgba(7, 10, 8, 0.151)
+  );
+  border-radius: 15%;
+
+}
+
+.nav_card_text {
+  color:rgb(253, 245, 232);
+  text-align: center;
+  font-size: 20px;
+  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
 }
 
 .logo {
