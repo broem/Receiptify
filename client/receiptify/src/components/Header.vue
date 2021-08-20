4f4edd74910a4f4d54bee169e71f586449fafe11
dlm1337 <61361992+dlm1337@users.noreply.github.com>
2021-08-19 20:53:23 -0400

    Update Header.vue

--- client/receiptify/src/components/Header.vue
+++ client/receiptify/src/components/Header.vue
@@ -1,125 +1,190 @@
 <template>
-<div id="header"> 
-      <img class="logo" @click="pushLandingPath()" src="@/assets/receiptifyLogo.png" /> 
-     <div class="nav">
-      <v-row no-gutters>
-      <v-col v-for="(k,i) in navBar.title" :key="i" cols="12" sm="2">
-        <v-card class="pa-2 nav_card" @click="pushNavPath(i)">
-            <div class="nav_card_text">{{k}}</div> 
-        </v-card>
-      </v-col>
-      <v-row>
-    </div> 
-   </div>
-      
+  <div id="header">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+
+    <div class="topnav" id="myTopnav">
+      <img class="logo" src="@/assets/receiptifyLogo.png" />
+
+      <a @click="pushLandingPath()" class="active" v-if="landingActive">Home</a>
+      <a @click="pushLandingPath()" v-if="!landingActive">Home</a>
+
+      <a @click="pushPremiumPath()" class="active" v-if="premiumActive"
+        >Premium</a
+      >
+      <a @click="pushPremiumPath()" v-if="!premiumActive">Premium</a>
+
+      <a @click="pushExplorePath()" class="active" v-if="exploreActive"
+        >Explore</a
+      >
+      <a @click="pushExplorePath()" v-if="!exploreActive">Explore</a>
+
+      <a @click="pushProductPath()" class="active" v-if="productActive"
+        >Product</a
+      >
+      <a @click="pushProductPath()" v-if="!productActive">Product</a>
+
+      <a @click="pushDeveloperPath()" class="active" v-if="developerActive"
+        >Developer</a
+      >
+      <a @click="pushDeveloperPath()" v-if="!developerActive">Developer</a>
+
+      <a @click="pushSignInPath()" class="active" v-if="signInActive"
+        >Sign In</a
+      >
+      <a @click="pushSignInPath()" v-if="!signInActive">Sign In</a>
+
+      <a href="javascript:void(0);" class="icon" @click="myFunction()">
+        <i class="fa fa-bars"></i
+      ></a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name:"header",
+  name: "header",
   data: () => ({
-      //Just add title[x] and route[x] to object below, and it will add to nav-bar. :)
-    navBar:{
-      title: ["Premium", "Explore", "Product", "Developer", "Sign In"],
-      route: ["/premium", "/explore", "/product", "/developer", "/signIn"]
-    } 
+    landingActive: true,
+    premiumActive: false,
+    exploreActive: false,
+    productActive: false,
+    developerActive: false,
+    signInActive: false,
   }),
   methods: {
-   pushNavPath(i) {
-     this.$router.push(this.navBar.route[i]);
-   },
-   pushLandingPath() {
-     this.$router.push("/");
-   }
+    pushLandingPath() {
+      this.$router.push("/");
+      this.landingActive = true;
+      this.premiumActive = false;
+      this.exploreActive = false;
+      this.productActive = false;
+      this.developerActive = false;
+      this.signInActive = false;
+    },
+    pushPremiumPath() {
+      this.$router.push("/premium");
+      this.landingActive = false;
+      this.premiumActive = true;
+      this.exploreActive = false;
+      this.productActive = false;
+      this.developerActive = false;
+      this.signInActive = false;
+    },
+    pushExplorePath() {
+      this.$router.push("/explore");
+      this.landingActive = false;
+      this.premiumActive = false;
+      this.exploreActive = true;
+      this.productActive = false;
+      this.developerActive = false;
+      this.signInActive = false;
+    },
+    pushProductPath() {
+      this.$router.push("/product");
+      this.landingActive = false;
+      this.premiumActive = false;
+      this.exploreActive = false;
+      this.productActive = true;
+      this.developerActive = false;
+      this.signInActive = false;
+    },
+    pushDeveloperPath() {
+      this.$router.push("/developer");
+      this.landingActive = false;
+      this.premiumActive = false;
+      this.exploreActive = false;
+      this.productActive = false;
+      this.developerActive = true;
+      this.signInActive = false;
+    },
+    pushSignInPath() {
+      this.$router.push("/signIn");
+      this.landingActive = false;
+      this.premiumActive = false;
+      this.exploreActive = false;
+      this.productActive = false;
+      this.developerActive = false;
+      this.signInActive = true;
+    },
+    myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    },
   },
-}
+};
 </script>
 
 <style>
- #header {
-  position: relative; 
-  top: 0%;
-  width: 100%;
-  height: 4vh;
-  background-image: linear-gradient(
-    to right bottom,
-    rgba(9, 46, 28, 0.788),
-    rgba(6, 19, 5, 0.432)
-  );
-}
- 
- @media(max-width:599px)
-{
-    .logo {
-    width: 180px;
-    position: absolute;
-    left: 0%;  
-    z-index: 2;
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5), 10px 10px 15px rgba(0, 0, 0, 0.5),
-    -1px -1px 30px rgba(0, 0, 0, 0.2);
-    }
-    .nav { 
-    position: absolute; 
-    left: 75%;
-    width: 100px; 
-    object-fit: contain; 
-    z-index: 2;
-}
-  
+#header {
+  position: relative;
 }
 
-@media(min-width:600px)
-{
-    .logo {
-    width: 240px;
-    position: absolute;
-    left: 0%;  
-    z-index: 2;
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5), 10px 10px 15px rgba(0, 0, 0, 0.5),
-    -1px -1px 30px rgba(0, 0, 0, 0.2);
-    }  
-    .nav { 
-    position: absolute; 
-    width: 500px; 
-    left: 40%;
-    object-fit: contain;
-    z-index: 2;
-    }
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
 }
 
-@media(min-width:1100px)
-{
-    .logo {
-    width: 400px;
-    position: absolute;
-    left: 0%;  
-    z-index: 2;
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5), 10px 10px 15px rgba(0, 0, 0, 0.5),
-    -1px -1px 30px rgba(0, 0, 0, 0.2);
-    }  
-    .nav { 
-    position: absolute; 
-    width: 800px; 
-    left: 50%;
-    object-fit: contain;
-    z-index: 2;
-    }
-}
- 
- .nav_card {
-  background-image: linear-gradient(
-    to right bottom,
-    rgba(5, 22, 22, 0.884),
-    rgba(6, 5, 24, 0.678)
-  );
-  border-radius: 15%;
-
-}
-
-.nav_card_text {
-  color:rgb(253, 245, 232);
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: right;
+  display: block;
+  color: #f2f2f2;
   text-align: center;
-  font-size: 12px;
-  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add an active class to highlight the current page */
+.topnav a.active {
+  background-color: #04aa6d;
+  color: white;
+}
+
+/* Hide the link that should open and close the topnav on small screens */
+.topnav .icon {
+  display: none;
+}
+.logo {
+  float: left !important;
+  width: 200px;
+  z-index: 2;
+}
+/* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
+@media screen and (max-width: 600px) {
+  .topnav a:not(:first-child) {
+    display: none;
+  }
+  .topnav a.icon {
+    float: right;
+    display: block;
+  }
+  .topnav.responsive {
+    position: relative;
+  }
+  .topnav.responsive a.icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .topnav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
 }
 </style>
